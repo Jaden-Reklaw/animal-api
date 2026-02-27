ce8dfe975327427a178a4a1df7d47048952d3bfd
Jaden-Reklaw <jwalker117@gmail.com>
2026-02-27 14:56:28 -0600

    basic lambda restapi and dynamodb configuration on cdk project.

--- src/services/hello.js
+++ src/services/hello.js
@@ -1,6 +1,6 @@
 exports.main = async function(event, context) {
     return {
         statusCode: 200,
-        body: JSON.stringify({ message: 'Hello, World from Lambda!' }),
+        body: JSON.stringify({ message: `Hello, I will read from the ${process.env.TABLE_NAME}!` })
     };
 }
